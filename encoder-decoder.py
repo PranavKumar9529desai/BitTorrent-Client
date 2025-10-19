1f176293bd43ecbd2115c092e9256c6915b807ff
Your Name <fullstackwebdeveloper123@gmail.com>
2025-10-19 12:09:01 +0530

    feat : parsing the rorrent files

---
+++ encoder-decoder.py
@@ -0,0 +1,31 @@
+import bencode
+# r+ is for read and write
+# with open is used it automatically closes the file after the block
+
+with open('fedora-2.torrent' , 'rb') as file :
+    bencoded_content = file.read()
+
+    # Decoding the bencoded content
+    decoded_content = bencode.decode(bencoded_content)
+
+    # print 
+    print(type(decoded_content))
+    print(decoded_content.keys())
+
+    for key in decoded_content:
+        if key != 'info':
+            print(f"{key} : {decoded_content[key]}") 
+    info_dict = decoded_content['info']
+    print("Info Dictionary Keys : ", info_dict.keys())
+
+    # calculate the file size
+    for i in info_dict :
+        if i != "pieces" :
+            print(f"{i} : {info_dict[i]}")
+
+    file_dict = info_dict["files"]
+    size = 0
+    for fl in file_dict :
+        size+=fl["length"]
+    
+    print("size",size)
